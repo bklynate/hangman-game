afca549b39b62eeab14964ff2ce8d0d238bc0322
bklynate <nathanielcfa@gmail.com>
2016-11-17 21:32:22 -0500

    finshed logic in terminal

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,16 +8,16 @@ var wordbank = [
   'check',
   'coins',
   'credit',
-  'credit card',
   'currency',
   'customer',
   'deposit',
   'interest'
-]
+];
+
 var randomWordIndex = Math.floor(Math.random() * wordbank.length);
 var chosenWord = wordbank[randomWordIndex];
-var chosenWordArr = chosenWord.split("")
-var displayArr =[]
+var chosenWordArr = chosenWord.split("");
+var displayArr = [];
 
 for(var i=0; i < chosenWordArr.length; i++) {
   displayArr.push(chosenWordArr[i].replace(chosenWordArr[i],"_"));
@@ -25,6 +25,41 @@ for(var i=0; i < chosenWordArr.length; i++) {
 
 console.log(displayArr.join(" "));
 
-document.onkeyup = function(event) {
-  console.log(event.key);
+var maxCount = chosenWord.length;
+var count = 0;
+
+console.log(chosenWord);
+
+document.onkeydown = function(event) {
+  function checkForWinner(){
+    if(count === maxCount) {
+      document.onkeydown = null
+      if(displayArr.indexOf("_") !== -1) {
+        console.log("You Lose!");
+      }
+      if(displayArr.indexOf("_") === -1) {
+        console.log("You Won!");
+      }
+    }
+  }
+
+  var keyPress = event.key;
+
+  if(count !== maxCount) {
+    for(var i=0; i < chosenWord.length; i++) {
+      if(keyPress === chosenWord.charAt(i)) {
+        count++
+        console.log("Correct!");
+        displayArr[i] = keyPress;
+        console.log(displayArr.join(" "));
+        checkForWinner();
+      }
+    }
+
+    if(chosenWord.indexOf(keyPress) === -1) {
+      count++
+      console.log("Wrong !");
+      checkForWinner();
+    }
+  }
 }
