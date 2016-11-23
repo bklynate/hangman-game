a0adb7a654fe04b20e346ee7e11d2b9f2989463b
bklynate <nathanielcfa@gmail.com>
2016-11-22 21:29:35 -0500

    Add css to #game to help scroll effect

--- assets/js/game.js
+++ assets/js/game.js
@@ -51,78 +51,80 @@ var gameSounds = {
   }
 }
 
-var randomWordIndex = Math.floor(Math.random() * wordbank.length);
-var chosenWord = wordbank[randomWordIndex];
-var chosenWordArr = chosenWord.split("");
-var displayArr = [];
-var lettersPickedByUser = [];
+$(document).ready(function(){
+  var randomWordIndex = Math.floor(Math.random() * wordbank.length);
+  var chosenWord = wordbank[randomWordIndex];
+  var chosenWordArr = chosenWord.split("");
+  var displayArr = [];
+  var lettersPickedByUser = [];
 
-for(var i=0; i < chosenWordArr.length; i++) {
-  displayArr.push(chosenWordArr[i].replace(chosenWordArr[i],"_"));
-}
+  for(var i=0; i < chosenWordArr.length; i++) {
+    displayArr.push(chosenWordArr[i].replace(chosenWordArr[i],"_"));
+  }
 
-document.querySelector("#display").innerHTML = displayArr.join(" ");
+  document.querySelector("#display").innerHTML = displayArr.join(" ");
 
-var maxCount = 12;
-var count = 0;
-var guessesLeft = maxCount - count;
-document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-
-console.log("Spolier Alert: The word is ", chosenWord);
-
-document.onkeydown = function(event) {
+  var maxCount = 12;
+  var count = 0;
   var guessesLeft = maxCount - count;
   document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-  function checkForWinner() {
-    if(displayArr.indexOf("_") !== -1) {
-      if(count === maxCount) {
+
+  console.log("Spolier Alert: The word is ", chosenWord);
+
+  document.onkeydown = function(event) {
+    var guessesLeft = maxCount - count;
+    document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+    function checkForWinner() {
+      if(displayArr.indexOf("_") !== -1) {
+        if(count === maxCount) {
+          document.onkeydown = null
+          document.querySelector("#winOrLose").innerHTML = "<h3>You Lose!</h3>";
+          gameSounds.loser.sound.play();
+          restartGame();
+        }
+      }
+
+      if(displayArr.indexOf("_") === -1) {
         document.onkeydown = null
-        document.querySelector("#winOrLose").innerHTML = "<h3>You Lose!</h3>";
-        gameSounds.loser.sound.play();
+        document.querySelector("#winOrLose").innerHTML = "<h3>You Won!</h3>";
+        gameSounds.winner.sound.play();
         restartGame();
       }
     }
 
-    if(displayArr.indexOf("_") === -1) {
-      document.onkeydown = null
-      document.querySelector("#winOrLose").innerHTML = "<h3>You Won!</h3>";
-      gameSounds.winner.sound.play();
-      restartGame();
+    function restartGame() {
+      setTimeout(function() {
+        location.reload()
+      }, 2300);
     }
-  }
 
-  function restartGame() {
-    setTimeout(function() {
-      location.reload()
-    }, 2300);
-  }
+    if(event.keyCode >= 65 && event.keyCode <= 90) {
+      var keyPress = event.key;
+      if(count !== maxCount) {
+        for(var i=0; i < chosenWord.length; i++) {
+          if(keyPress === chosenWord.charAt(i)) {
+            count++
+            guessesLeft--
+            document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+            document.querySelector("#guessOutcome").innerHTML = "Correct !";
+            gameSounds.right.sound.play();
+            displayArr[i] = keyPress;
+            document.querySelector("#display").innerHTML = displayArr.join(" ");
+            checkForWinner();
+          }
+        }
 
-  if(event.keyCode >= 65 && event.keyCode <= 90) {
-    var keyPress = event.key;
-    if(count !== maxCount) {
-      for(var i=0; i < chosenWord.length; i++) {
-        if(keyPress === chosenWord.charAt(i)) {
-          count++
-          guessesLeft--
+        if(chosenWord.indexOf(keyPress) === -1) {
+          count++;
+          guessesLeft--;
+          lettersPickedByUser.push(keyPress);
+          document.querySelector("#pickedLettersArr").innerHTML = lettersPickedByUser.join(" ");
           document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-          document.querySelector("#guessOutcome").innerHTML = "Correct !";
-          gameSounds.right.sound.play();
-          displayArr[i] = keyPress;
-          document.querySelector("#display").innerHTML = displayArr.join(" ");
+          document.querySelector("#guessOutcome").innerHTML = "Wrong !";
+          gameSounds.wrong.sound.play();
           checkForWinner();
         }
       }
-
-      if(chosenWord.indexOf(keyPress) === -1) {
-        count++;
-        guessesLeft--;
-        lettersPickedByUser.push(keyPress);
-        document.querySelector("#pickedLettersArr").innerHTML = lettersPickedByUser.join(" ");
-        document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-        document.querySelector("#guessOutcome").innerHTML = "Wrong !";
-        gameSounds.wrong.sound.play();
-        checkForWinner();
-      }
     }
   }
-}
+})
